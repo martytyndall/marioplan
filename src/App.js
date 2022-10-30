ef488f42a3eb9dbf094691a4c52c9d8990305dbb
Marty Tyndall <m.tyndall1979@gmail.com>
2022-10-30 20:15:25 +0000

    First commit, lesson 5 Issues: content not displaying in browser.

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Navbar from './components/layout/Navbar'
+import Dashboard from './components/dashboard/Dashboard'
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />
+          <Routes>
+            <Route path='/'component={Dashboard} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/layout/SignedOutLinks.js
@@ -0,0 +1,14 @@
+import { NavLink } from 'react-router-dom'
+
+const SignedOutLinks = () => {
+  return (
+    <div>
+      <ul className="right">
+        <li><NavLink to='/'>Sign Up</NavLink></li>
+        <li><NavLink to='/'>Log In</NavLink></li>
+      </ul>
+    </div>
+  )
+}
+
+export default SignedOutLinks
