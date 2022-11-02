3fa9312450ce5371ef26942b5e3cd80a56d4e49e
Marty Tyndall <m.tyndall1979@gmail.com>
2022-11-02 08:04:15 +0000

    lesson 10

--- src/components/layout/SignedOutLinks.js
+++ src/components/layout/SignedOutLinks.js
@@ -4,8 +4,8 @@ const SignedOutLinks = () => {
   return (
     <div>
       <ul className="right">
-        <li><NavLink to='/'>Sign Up</NavLink></li>
-        <li><NavLink to='/'>Log In</NavLink></li>
+        <li><NavLink to='/signup'>Sign Up</NavLink></li>
+        <li><NavLink to='/signin'>Log In</NavLink></li>
       </ul>
     </div>
   )
