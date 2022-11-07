41e4e637863a567872039e92c02a25cd6c53db5b
Marty Tyndall <m.tyndall1979@gmail.com>
2022-11-07 07:59:38 +0000

    lesson 11 - Created authReducer to handle authentication, and projectReducer to handle the projects in Redux. - Created rootReducer to combine previous reducers. - Attached rootReducer to project by creating a store in index.js and attching store to <App/>

--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { legacy_createStore as createStore } from 'redux';
+import rootReducer from './store/reducers/rootReducer'
+import { Provider } from 'react-redux';
+
+const store = createStore(rootReducer);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <Provider store={store}><App /></Provider>
 );
